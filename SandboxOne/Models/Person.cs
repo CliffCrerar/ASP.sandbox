5bc0a944dc061ae3a8ed31ee7ab46d6a38487a53
Clifford Crerar <Cliffc@agilebridge.co.za>
2020-04-22 15:42:29 +0200

    first commit

---
+++ SandboxOne/Models/Person.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SandboxOne.Models
+{
+    public class Person
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class PersonValidator : AbstractValidator<Person>
+    {
+        public PersonValidator()
+        {
+            RuleFor(x => x.Id).NotNull();
+            RuleFor(x => x.FirstName).Length(0, 10);
+            RuleFor(x => x.LastName).Length(0, 10);
+            RuleFor(x => x.Age).InclusiveBetween(16, 18);
+        }
+    }
+}
---
+++ SandboxOne/Pages/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using SandboxOne.Models;
+
+namespace SandboxOne.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public Person Person { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+            Person = new Person();
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+
+        }
+    }
+}
